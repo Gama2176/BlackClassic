01dac67387b1345fc572dcbd2160b08616c84b21
Gironmv12 <gironsan08918@gmail.com>
2025-03-04 19:06:40 -0600

    correccion rutas app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
 import LandingPage from './pages/public/LandingPage';
-import Encuentranos from "./pages/Encuentranos"
-import Galeria from "./pages/Galeria";
+
 
 const App = () => {
   return (
     <>
     <LandingPage />
-      <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/novedades" element={<Novedades />} />
-        <Route path="/encuentranos" element={<Encuentranos />} />
-        <Route path="/galeria" element={<Galeria />} />
-      </Routes>
-      <Footer />
-    </Router>
     </>
   );
 };
